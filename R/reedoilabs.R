5b6cdadfe3d0b083c3fcc347d7c819a3efdf6b9e
Chester Ismay <chester.ismay@gmail.com>
2016-08-17 16:26:18 -0700

    Removed imports from reedoilabs.R

---
+++ R/reedoilabs.R
@@ -0,0 +1,12 @@
+#' reedoilabs: An R package accompanying the OpenIntro labs for Reed College.
+#' 
+#' The reedoilabs package contains the data sets, custom functions, documentation, and templates for the OpenIntro Labs for Reed College.
+#' 
+#' In particular, it contains the datasets arbuthnot, atheism, bdims, cdc, evals, kobe, mlb11 and present. 
+#' 
+#' It also contains the functions inference, multiLines, plot_ss and qqnormsim.
+#' 
+#'  @docType package
+#'  @name reedoilabs
+NULL
+#> NULL
